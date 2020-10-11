dda290f9cf851594dc9436b863d3a5660019a7b1
Sellers Crisp <sellershcrisp@gmail.com>
2020-10-11 14:39:37 -0500

    updated app.js after finishing the exercise

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 let facts = { numPlanets: 8, yearNeptuneDiscovered: 1846 };
 let { numPlanets, yearNeptuneDiscovered } = facts;
 
-console.log(numPlanets); // ?
-console.log(yearNeptuneDiscovered); // ?
+console.log(numPlanets); // 8
+console.log(yearNeptuneDiscovered); // 1846
 
 // Object Destructuring 2
 let planetFacts = {
@@ -14,23 +14,23 @@ let planetFacts = {
 
 let { numPlanets, ...discoveryYears } = planetFacts;
 
-console.log(discoveryYears); // ?
+console.log(discoveryYears); // {yearNeptuneDiscovered: 1846, yearMarsDiscovered: 1659}
 
 // Object Destructuring 3
 function getUserData({ firstName, favoriteColor = 'green' }) {
   return `Your name is ${firstName} and you like ${favoriteColor}`;
 }
 
-getUserData({ firstName: 'Alejandro', favoriteColor: 'purple' }); // ?
-getUserData({ firstName: 'Melissa' }); // ?
-getUserData({}); // ?
+getUserData({ firstName: 'Alejandro', favoriteColor: 'purple' }); // Your name is Alejandro and you like purple
+getUserData({ firstName: 'Melissa' }); // Your name is Melissa and you like green
+getUserData({}); // Your name is undefined and you like green
 
 // Array Destructuring 1
 let [first, second, third] = ['Maya', 'Marisa', 'Chi'];
 
-console.log(first); // ?
-console.log(second); // ?
-console.log(third); // ?
+console.log(first); // Maya
+console.log(second); // Marisa
+console.log(third); // Chi
 
 // Array Destructuring 2
 let [raindrops, whiskers, ...aFewOfMyFavoriteThings] = [
@@ -41,15 +41,15 @@ let [raindrops, whiskers, ...aFewOfMyFavoriteThings] = [
   'Brown paper packages tied up with strings',
 ];
 
-console.log(raindrops); // ?
-console.log(whiskers); // ?
-console.log(aFewOfMyFavoriteThings); // ?
+console.log(raindrops); // Raindrops on roses
+console.log(whiskers); // whiskers on kittens
+console.log(aFewOfMyFavoriteThings); // ["Bright copper kettles", "warm woolen mittens", "Brown paper packages tied up with strings"]
 
 // Array Destructuring 3
 let numbers = [10, 20, 30];
 [numbers[1], numbers[2]] = [numbers[2], numbers[1]];
 
-console.log(numbers); // ?
+console.log(numbers); // [10, 30, 20]
 
 // ES2015 Refactoring
 // ES5 Assigning Variables to Object Properties
@@ -64,6 +64,13 @@ var a = obj.numbers.a;
 var b = obj.numbers.b;
 
 /* Write an ES2015 Version */
+const obj = {
+  numbers: {
+    a: 1,
+    b: 2,
+  },
+};
+const { a, b } = obj.numbers;
 
 // ES5 Array Swap
 var arr = [1, 2];
@@ -72,6 +79,7 @@ arr[0] = arr[1];
 arr[1] = temp;
 
 /* Write an ES2015 Version */
+[arr[0], arr[1]] = [arr[1], arr[0]];
 
 // raceResults()
 // Write a function called raceResults which accepts a single array argument.It should return an object with the keys first, second, third, and rest.
@@ -94,3 +102,11 @@ raceResults(['Tom', 'Margaret', 'Allison', 'David', 'Pierre']);
     rest: ["David", "Pierre"]
   }
 */
+
+// supposed to be a oneliner but Prettier formatting messes up the raceResults object ):
+const raceResults = ([first, second, third, ...rest]) => ({
+  first,
+  second,
+  third,
+  rest,
+});
